e18db9c629f99cb5b99ef7e2c97a7acc04a03924
totallynotbuzzismaloy <121229165+totallynotbuzzismaloy@users.noreply.github.com>
2023-02-20 01:43:17 +0300

    Add files via upload

---
+++ OS second lab/OS second lab/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace OS_second_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 73);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(348, 414);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(380, 73);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(215, 122);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "logic disks";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(380, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "show";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "name of the user:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button5.Location = new System.Drawing.Point(496, 211);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 54);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "clear";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button6.Location = new System.Drawing.Point(496, 25);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 25);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "clear";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(380, 282);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(215, 129);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(380, 425);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 62);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "RAM";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button8.Location = new System.Drawing.Point(496, 425);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 62);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Clear";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(912, 613);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+
+        private System.Windows.Forms.TextBox textBox4;
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.TextBox textBox3;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ OS second lab/OS second lab/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace OS_second_lab
+{
+    public partial class Form1 : Form
+    {
+        private DriveInfo[] alldrives = DriveInfo.GetDrives();
+        private ManagementObjectSearcher Search = new ManagementObjectSearcher("Select * From Win32_ComputerSystem");
+        private System.Timers.Timer my_timer;
+        private delegate void my_delegate();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void output_info()
+        {
+            textBox1.Clear();
+            foreach (DriveInfo drive in alldrives)
+            {
+                textBox1.Text = "Drive " + drive.Name + Environment.NewLine;
+                textBox1.Text = textBox1.Text + "   Drive type: " + drive.DriveType + Environment.NewLine;
+                if (drive.IsReady == true)
+                {
+                    textBox1.Text = textBox1.Text + "   Volume Label is " + drive.VolumeLabel + Environment.NewLine;
+                    textBox1.Text = textBox1.Text + "   File system is " + drive.DriveFormat + Environment.NewLine;
+                    textBox1.Text = textBox1.Text + $"   Available space is {drive.AvailableFreeSpace, 15} bytes" 
+                                                  + Environment.NewLine;
+                    textBox1.Text = textBox1.Text + $"   Total available space is {drive.TotalFreeSpace, 15} bytes"
+                                                  + Environment.NewLine;
+                    textBox1.Text = textBox1.Text +
+                                    $"   Total size of drive is {Math.Round(drive.TotalSize / 1073741824.0, 3), 8} Gbytes"
+                                    + Environment.NewLine;
+                }
+            }
+        }
+        
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String[] drives = Environment.GetLogicalDrives();
+            textBox2.Text = "The list of names of logical drives: " + String.Join(", ", drives);
+            textBox2.Text = textBox2.Text + Environment.NewLine + "The number of logical drives is" + drives.Length;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Environment.UserName;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            foreach (ManagementObject Mobject in Search.Get())
+            {
+                double Ram_bytes = Convert.ToDouble(Mobject["TotalPhysicalMemory"]);
+                textBox4.Text = "RAM size in bytes is " + Ram_bytes + " bytes";
+                textBox4.Text = textBox4.Text + Environment.NewLine + "RAM size in Gbytes is " +
+                                Math.Round(Ram_bytes / 1073741824.0, 3) + " Gbytes";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            my_timer = new System.Timers.Timer();
+            my_timer.Interval = 200;
+            my_timer.Elapsed += elapsed_event;
+            my_timer.Start(); 
+            
+        }
+
+        private void elapsed_event(object sender, EventArgs e)
+        {
+            textBox1.BeginInvoke(new my_delegate(output_info));
+        }
+        
+    }
+    
+}
